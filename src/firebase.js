9d9900f9c9946dc231ab6fb6340819417dcce3e0
mishrashubham17 <shubhammishra69897@gmail.com>
2021-09-22 15:08:58 +0530

    basic setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDxToemvrVvY5Bw3X-ZNADQW3pVOTnx5SM",
+    authDomain: "netflix-clone-ace4d.firebaseapp.com",
+    projectId: "netflix-clone-ace4d",
+    storageBucket: "netflix-clone-ace4d.appspot.com",
+    messagingSenderId: "718752376863",
+    appId: "1:718752376863:web:80f81ba73b631f362a0d2a"
+  };
